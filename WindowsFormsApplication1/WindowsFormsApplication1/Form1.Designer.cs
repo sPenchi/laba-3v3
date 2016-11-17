2d36df8ed21ee94fb50cc1f16947ed9c870da216
sPenchi <mr.pronenko@bk.ru>
2016-11-17 17:57:41 +0300

    "Добавил 2 кнопки корень и степень"

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.T2_textBox2 = new System.Windows.Forms.TextBox();
             this.T2_textBox1 = new System.Windows.Forms.TextBox();
             this.T2_textBox3 = new System.Windows.Forms.TextBox();
+            this.T2_button6 = new System.Windows.Forms.Button();
+            this.T2_button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // T2_button4
@@ -75,11 +77,31 @@
             this.T2_textBox3.Size = new System.Drawing.Size(262, 20);
             this.T2_textBox3.TabIndex = 12;
             // 
+            // T2_button6
+            // 
+            this.T2_button6.Location = new System.Drawing.Point(329, 398);
+            this.T2_button6.Name = "T2_button6";
+            this.T2_button6.Size = new System.Drawing.Size(75, 23);
+            this.T2_button6.TabIndex = 14;
+            this.T2_button6.Text = "Корень";
+            this.T2_button6.UseVisualStyleBackColor = true;
+            // 
+            // T2_button3
+            // 
+            this.T2_button3.Location = new System.Drawing.Point(66, 398);
+            this.T2_button3.Name = "T2_button3";
+            this.T2_button3.Size = new System.Drawing.Size(75, 22);
+            this.T2_button3.TabIndex = 13;
+            this.T2_button3.Text = "В степень";
+            this.T2_button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(480, 523);
+            this.Controls.Add(this.T2_button6);
+            this.Controls.Add(this.T2_button3);
             this.Controls.Add(this.T2_textBox3);
             this.Controls.Add(this.T2_button4);
             this.Controls.Add(this.T2_button1);
@@ -99,6 +121,8 @@
         private System.Windows.Forms.TextBox T2_textBox2;
         private System.Windows.Forms.TextBox T2_textBox1;
         private System.Windows.Forms.TextBox T2_textBox3;
+        private System.Windows.Forms.Button T2_button6;
+        private System.Windows.Forms.Button T2_button3;
     }
 }
 
